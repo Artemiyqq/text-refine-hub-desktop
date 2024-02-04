3dfce98c69cb3b3bcb4e8775abd14b1bf3bba0a9
ArtemFlorko <artom.florko@gmail.com>
2024-02-04 14:04:15 +0200

    The logic of text processing and delivering results

--- TextRefineHubDesktop/Views/TextProcessingPage.xaml.cs
+++ TextRefineHubDesktop/Views/TextProcessingPage.xaml.cs
@@ -1,3 +1,4 @@
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace TextRefineHubDesktop.Views
@@ -10,6 +11,28 @@ namespace TextRefineHubDesktop.Views
         public TextProcessingPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = TextBoxControl;
+
+            if (textBox != null)
+            {
+                string inputText = textBox.Text;
+                string processedText = App.TextProcessingService.ProcessText(inputText);
+
+                var dataPackage = new Windows.ApplicationModel.DataTransfer.DataPackage();
+                dataPackage.SetText(processedText);
+                Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(dataPackage);
+
+                var flyout = new Flyout();
+                var flyoutContent = new TextBlock { Text = "Result copied to clipboard!" };
+
+                flyout.Content = flyoutContent;
+
+                flyout.ShowAt((FrameworkElement)sender);
+            }
         }
     }
 }
